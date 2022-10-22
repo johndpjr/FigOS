cc4524ce53dae4da3fd55eef8bf955d858afd19a
johndpjr <johndpowell02@gmail.com>
2022-10-22 00:23:07 -0500

    origin 0 & add segment registers

--- boot.asm
+++ boot.asm
@@ -1,11 +1,32 @@
 ; Bootloader for PeachOS
 
 ; begin at 0x7c00 (set origin)
-ORG 0x7c00
+ORG 0
 ; 16-bit architecture (use 16-bit assembly instructions)
 BITS 16
+_start:
+    jmp short start
+    nop
+
+; BIOS parameter block
+times 33 db 0
 
 start:
+    ; code segment becomes 0x7c0
+    jmp 0x7c0:step2
+
+step2:
+    ; clear interrupts (about to change segment registers)
+    cli
+    ; set 0x7c0 as the data segment register
+    mov ax, 0x7c0
+    mov ds, ax
+    mov es, ax
+    mov ax, 0x00
+    mov ss, ax
+    mov sp, 0x7c00 ; data segment
+    ; enable interrupts
+    sti
     ; si is index register
     mov si, message
     call print
