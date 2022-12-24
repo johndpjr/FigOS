b6b52605eacdb17e34cb7734fd684ca5d5be26e9
johndpjr <johndpowell02@gmail.com>
2022-12-24 10:44:12 -0600

    fix overflow on diskstreamer_read

--- src/disk/streamer.c
+++ src/disk/streamer.c
@@ -1,5 +1,7 @@
 #include "streamer.h"
 
+#include <stdbool.h>
+
 #include "config.h"
 #include "memory/heap/kheap.h"
 
@@ -25,20 +27,26 @@ int diskstreamer_read(struct disk_stream* stream, void* out, int total)
 {
     int sector = stream->pos / PEACHOS_SECTOR_SIZE;
     int offset = stream->pos % PEACHOS_SECTOR_SIZE;
+    int total_to_read = total;
+    bool overflow = (offset + total_to_read) >= PEACHOS_SECTOR_SIZE;
     char buf[PEACHOS_SECTOR_SIZE];
+
+    if (overflow) {
+        total_to_read -= (offset + total_to_read) - PEACHOS_SECTOR_SIZE;
+    }
 
     int res = disk_read_block(stream->disk, sector, 1, buf);
     if (res < 0)
         goto out;
 
-    int total_to_read = total > PEACHOS_SECTOR_SIZE ? PEACHOS_SECTOR_SIZE : total;
     for (int i = 0; i < total_to_read; ++i)
         *(char*)out++ = buf[offset+i];
 
     // Adjust the stream
     stream->pos += total_to_read;
-    if (total > PEACHOS_SECTOR_SIZE)
-        res = diskstreamer_read(stream, out, total-PEACHOS_SECTOR_SIZE);
+    if (overflow)
+        res = diskstreamer_read(stream, out, total-total_to_read);
+
 out:
     return res;
 }
