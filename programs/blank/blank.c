39f514a9059f24063b26d301a8bbbe2bac974118
johndpjr <johndpowell02@gmail.com>
2022-12-26 17:09:59 -0600

    port print and getkey into stdlib

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -1,4 +1,12 @@
+#include "peachos.h"
+
 int main(int argc, char** argv)
 {
+    print("Hello how are you?\n");
+
+    while (1) {
+        if (getkey() != 0)
+            print("Key was pressed\n");
+    }
     return 0;
 }
---
+++ programs/stdlib/src/peachos.h
@@ -0,0 +1,7 @@
+#ifndef PEACHOS_PEACHOS_H
+#define PEACHOS_PEACHOS_H
+
+void print(const char* message);
+int getkey();
+
+#endif //PEACHOS_PEACHOS_H
