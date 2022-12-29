7ec8bebd5f0c0665e5a227798632e849d7f5e70a
johndpjr <johndpowell02@gmail.com>
2022-12-28 18:44:52 -0600

    FAT16: fix bug

--- src/fs/fat/fat16.c
+++ src/fs/fat/fat16.c
@@ -502,9 +502,10 @@ struct fat_item* fat16_new_fat_item_for_directory_item(struct disk* disk, struct
     if (!f_item)
         return 0;
     
-    if (item->attribute && FAT_FILE_SUBDIRECTORY) {
+    if (item->attribute & FAT_FILE_SUBDIRECTORY) {
         f_item->directory = fat16_load_fat_directory(disk, item);
         f_item->type = FAT_ITEM_TYPE_DIRECTORY;
+        return f_item;
     }
 
     f_item->type = FAT_ITEM_TYPE_FILE;
